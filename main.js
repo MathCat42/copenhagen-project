4cd8ef2a3d029b144632d374c2de98bc2311bf51
MathCat42 <95234144+MathCat42@users.noreply.github.com>
2022-12-10 07:11:08 -0600

    modify

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function init() {
-    
+
 }
 var questions = []
 questions[0] = {options: ["","","",""], ans: 3}
@@ -16,7 +16,11 @@ questions[10] = {options: ["","","",""], ans: 4}
 questions[11] = {options: ["","","",""], ans: 2}
 questions[12] = {options: ["","","",""], ans: 1}
 questions[13] = {options: ["","","",""], ans: 1}
-document.getElementById("option1").style.display = "none"
-document.getElementById("option2").style.display = "none"
-document.getElementById("option3").style.display = "none"
-document.getElementById("option4").style.display = "none"
+var option1 = document.getElementById("option1")
+option1.style.display = "none"
+var option2 = document.getElementById("option2")
+option2.style.display = "none"
+var option3 = document.getElementById("option3")
+option3.style.display = "none"
+var option4 = document.getElementById("option4")
+option4.style.display = "none"
